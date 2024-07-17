7a63dd843e8e341bd1a0459dc030ad3738ca248b
Arthur Thomas <athomas@sandscap.com>
2024-07-17 19:00:34 -0400

    #66 wip: add ability to create key versions

---
+++ KeyVaultExplorer/Views/Pages/PropertiesDialogs/CreateNewKeyVersion.axaml.cs
@@ -0,0 +1,31 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+
+
+using KeyVaultExplorer.ViewModels;
+
+namespace KeyVaultExplorer;
+
+public partial class CreateNewKeyVersion : UserControl
+{
+    public CreateNewKeyVersion()
+    {
+        InitializeComponent();
+        DataContext = new CreateNewKeyVersionViewModel();
+    }
+
+    private void InputField_OnAttachedToVisualTree(object sender, VisualTreeAttachmentEventArgs e)
+    {
+        // We will set the focus into our input field just after it got attached to the visual tree.
+        if (sender is InputElement inputElement)
+        {
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                inputElement.Focus(NavigationMethod.Unspecified, KeyModifiers.None);
+            });
+        }
+    }
+}
