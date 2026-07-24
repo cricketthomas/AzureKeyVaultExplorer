516712d5b8acb94e1215535299f920737052243c
cricketthomas <arthurthomasiv@gmail.com>
2026-07-24 08:11:46 -0400

    update pipeline with sha for homebrew cask support (eventually) (#178) [skip ci]
    update publish store pipeline to build packages for store

--- src/uno/AzureKeyVaultStudio/msbuild.ps1
+++ src/uno/AzureKeyVaultStudio/msbuild.ps1
@@ -1,16 +1,11 @@
 
 param(
-    [string]$BuildNumber = '2.1.1.0'
+    [string]$BuildNumber = '2.1.1.0',
+    [string]$OutputPath = 'C:\temp\output'
 )
 
-msbuild .\AzureKeyVaultStudio.csproj /t:Restore,Build /r /p:TargetFramework='net10.0-windows10.0.26100' /p:Configuration=Release /p:Platform=x64 /p:GenerateAppxPackageOnBuild=true /p:AppxBundle=Never /p:UapAppxPackageBuildMode=StoreUpload  /p:AppxManifestIdentityVersion=$BuildNumber /p:AppxPackageDir="C:\temp\output\$BuildNumber";
+msbuild .\AzureKeyVaultStudio.csproj /t:Restore,Build /r /p:TargetFramework='net10.0-windows10.0.26100' /p:Configuration=Release /p:Platform=x64 /p:GenerateAppxPackageOnBuild=true /p:AppxBundle=Never /p:UapAppxPackageBuildMode=StoreUpload  /p:AppxManifestIdentityVersion=$BuildNumber /p:AppxPackageDir="$OutputPath\$BuildNumber";
 
-return
+msbuild .\AzureKeyVaultStudio.csproj /t:Restore,Build /r /p:TargetFramework='net10.0-windows10.0.26100' /p:Configuration=Release /p:Platform=x86 /p:GenerateAppxPackageOnBuild=true /p:AppxBundle=Never /p:UapAppxPackageBuildMode=StoreUpload  /p:AppxManifestIdentityVersion=$BuildNumber /p:AppxPackageDir="$OutputPath\$BuildNumber";    
 
-msbuild .\AzureKeyVaultStudio.csproj /t:Restore,Build /r /p:TargetFramework='net10.0-windows10.0.26100' /p:Configuration=Release /p:Platform=x86 /p:GenerateAppxPackageOnBuild=true /p:AppxBundle=Never /p:UapAppxPackageBuildMode=StoreUpload  /p:AppxManifestIdentityVersion=$BuildNumber /p:AppxPackageDir="C:\temp\output\$BuildNumber";    
-
-
-
-
-
-msbuild .\AzureKeyVaultStudio.csproj /t:Restore,Build /r /p:TargetFramework='net10.0-windows10.0.26100' /p:Configuration=Release /p:Platform=arm64 /p:GenerateAppxPackageOnBuild=true /p:AppxBundle=Never /p:UapAppxPackageBuildMode=StoreUpload  /p:AppxManifestIdentityVersion=$BuildNumber /p:AppxPackageDir="C:\temp\output\$BuildNumber";
+msbuild .\AzureKeyVaultStudio.csproj /t:Restore,Build /r /p:TargetFramework='net10.0-windows10.0.26100' /p:Configuration=Release /p:Platform=arm64 /p:GenerateAppxPackageOnBuild=true /p:AppxBundle=Never /p:UapAppxPackageBuildMode=StoreUpload  /p:AppxManifestIdentityVersion=$BuildNumber /p:AppxPackageDir="$OutputPath\$BuildNumber";
